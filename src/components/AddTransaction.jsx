4347cd955b0f5b163f9bc944065d23f1767680bc
Muskan Mittal <mittalmuskan758@gmail.com>
2023-04-05 17:21:32 +0530

    source code added

---
+++ src/components/AddTransaction.jsx
@@ -0,0 +1,42 @@
+import React, { useContext, useState } from "react";
+import { GlobalContext } from "../context/GlobalState";
+
+const AddTransaction = () => {
+    const [text, setText] = useState('');
+    const [amount, setAmount] = useState(0);
+
+    const { addTransactions } = useContext(GlobalContext);
+
+    const onSubmit = e => {
+        e.preventDefault();
+        const newTransaction = {
+            id: Math.floor(Math.random() * 100000000),
+            text,
+            amount: +amount
+        }
+
+        addTransactions(newTransaction);
+    }
+
+    return (
+        <div>
+            <h3> Add new transaction </h3>
+            <form onSubmit={onSubmit}>
+                <div className="form-control">
+                    <label htmlFor="text">Text</label>
+                    <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..."></input>
+                </div>
+                <div className="form-control">
+                    <label htmlFor="amount"
+                    > Amount <br />
+                    (negative - expense, positive - income)
+                    </label>
+                    <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount... "></input>
+                </div>
+                <button className="btn"> Add transaction </button>
+            </form>
+        </div>
+    )
+};
+
+export default AddTransaction;
---
+++ src/context/GlobalState.js
@@ -0,0 +1,40 @@
+import React, { createContext, useReducer } from "react";
+import AppReducer from "./AppReducer";
+
+// Initial State
+const initialState = {
+    transactions: []
+}
+
+// create context
+export const GlobalContext = createContext(initialState);
+
+// provider component
+export const GlobalProvider =  ({ children }) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    //Actions
+    function deleteTransactions(id) {
+        dispatch({
+            type: 'DELETE_TRANSACTION',
+            payload: id,
+        })
+    }
+
+    function addTransactions(transaction) {
+        dispatch({
+            type: 'ADD_TRANSACTION',
+            payload: transaction,
+        })
+    }
+
+    return (
+        <GlobalContext.Provider value={{
+            transactions: state.transactions,
+            deleteTransactions,
+            addTransactions
+        }}>
+            {children}
+        </GlobalContext.Provider>
+    )
+}
